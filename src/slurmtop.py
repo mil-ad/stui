8ff93d69f093c58d6ba4d91e92ac5b157822b7b9
Milad Alizadeh <milad2k+git@gmail.com>
2020-06-13 00:40:27 +0100

    Starting to add a list representing squeue

--- src/slurmtop.py
+++ src/slurmtop.py
@@ -1,6 +1,32 @@
 import urwid
 
-txt = urwid.Text(u"slurmtop")
-fill = urwid.Filler(txt, 'top')
-loop = urwid.MainLoop(fill)
-loop.run()
+def exit_on_q(key):
+    if key in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+
+def job_context_menu():
+    cancel_job = urwid.Button(u"Cancel Job")
+    back_button = urwid.Button(u"Back")
+
+    urwid.Pile([cancel_job, back_button])
+
+def job_list():
+    jobs = [u"job1", u"job2", u"job3"]
+
+    queue = []
+    for job in jobs:
+        b = urwid.Button(job)
+        queue.append(urwid.AttrWrap(b, None, "reversed"))
+        urwid.connect_signal(b, "click", job_context_menu)
+
+    lw = urwid.SimpleFocusListWalker(queue)
+    lb = urwid.ListBox(lw)
+
+    return lb
+
+if __name__ == "__main__":
+
+    lb = job_list()
+
+    loop = urwid.MainLoop(lb, palette=[('reversed', 'standout', '')], unhandled_input=exit_on_q)
+    loop.run()
