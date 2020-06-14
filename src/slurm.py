5ab0fca2090904aace2f672c510d0064b812bcb6
Milad Alizadeh <milad2k+git@gmail.com>
2020-06-14 15:18:27 +0100

    Getting the most basic job list via squeue

--- src/slurm.py
+++ src/slurm.py
@@ -1,8 +1,44 @@
+import subprocess
+
 class Cluster(object):
     def __init__(self):
         super().__init__()
 
+        self.me = None
+
+        # self.nodes = get_nodes()
+        self.partitions = None
+
+class Partition(object):
+    def __init__(self):
+        super().__init__()
 
 class Job(object):
     def __init__(self):
         super().__init__()
+
+        self.state = None
+
+class JobStep(object):
+    def __init__(self):
+        super().__init__()
+
+class Node(object):
+    def __init__(self):
+        super().__init__()
+
+
+
+def squeue_loop():
+    command = "ssh yarin squeue"
+    squeue_process = subprocess.run(command.split(" "), capture_output=True)
+
+    o = squeue_process.stdout.decode("utf-8").splitlines()
+
+    jobs = []
+    fields = o[0].split()
+    for line in o[1:]:
+        job = {k: v for k, v in zip(fields, line.split())}
+        jobs.append(job)
+
+    return jobs
