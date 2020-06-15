5bd2b6b5e293dfe10b7f52c236d45062d0035506
Milad Alizadeh <milad2k+git@gmail.com>
2020-06-15 11:19:21 +0100

    Added StyledLineBox. Looks good

--- src/slurmtop.py
+++ src/slurmtop.py
@@ -19,12 +19,29 @@ def menu_button(label, callback):
     button = JobWidget(label)
     # button = urwid.Text(label)
     # urwid.connect_signal(button, 'click', callback)
-    return urwid.AttrMap(button, None, focus_map='reversed')
+    return urwid.AttrMap(button, None, focus_map="reversed")
+
+
+class StyledLineBox(urwid.LineBox):
+    def __init__(self, original_widget, title):
+        super().__init__(
+            original_widget,
+            title,
+            title_align="left",
+            tlcorner="╭",
+            trcorner="╮",
+            blcorner="╰",
+            brcorner="╯",
+            tline="─",
+            bline="─",
+            lline="│",
+            rline="│",
+        )
+
 
 def menu(title, menu_items):
-    body = [urwid.Text(title), urwid.Divider()]
-    body.extend(menu_items)
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
+    return StyledLineBox(urwid.ListBox(urwid.SimpleFocusListWalker(menu_items)), title)
+
 
 def job_context_menu():
     cancel_job = urwid.Button(u"Cancel Job")
@@ -32,7 +49,8 @@ def job_context_menu():
 
     urwid.Pile([cancel_job, back_button])
 
-def job_list():
+
+def queue_panel():
     jobs = slurm.get_jobs()
 
     captions = [str(j) for j in jobs]
@@ -45,11 +63,19 @@ def job_list():
     #     menu_buttons.append(urwid.Columns([c1, c2]))
 
     menu_buttons = [menu_button(c, job_context_menu) for c in captions]
-    return menu("Job List", menu_buttons)
+    return menu("Queue", menu_buttons)
+
 
 if __name__ == "__main__":
 
-    lb = job_list()
+    qpanel = queue_panel()
+    options_panel = StyledLineBox(urwid.Filler(urwid.CheckBox("All")), "Options")
 
-    loop = urwid.MainLoop(lb, palette=[('reversed', 'standout', '')], unhandled_input=exit_on_q)
+    top_widget = urwid.Columns(
+        [("weight", 80, qpanel), ("weight", 20, options_panel)], dividechars=1
+    )
+
+    loop = urwid.MainLoop(
+        top_widget, palette=[("reversed", "standout", "")], unhandled_input=exit_on_q
+    )
     loop.run()
