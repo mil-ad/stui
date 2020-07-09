c846244b349bb832122d9d72473d8dd42d0f880d
Milad Alizadeh <milad2k+git@gmail.com>
2020-07-09 17:25:27 +0100

    Specify version for fabric. Not sure why I need this

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="stui",
     description="A Slurm client for the terminal.",
     long_description="A Slurm client for the terminal.", #TODO: Use readme
-    version="0.1.0",
+    version="0.1.4",
     packages=find_packages(),
     author="Milad Alizadeh",
     url="https://github.com/mi-lad/stui",
@@ -17,7 +17,7 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
     ],
-    install_requires=["urwid", "fabric"],
+    install_requires=["urwid", "fabric>=2.5.0"],
     python_requires=">=3.6",
     entry_points={"console_scripts": ["stui=stui.stui:main",]},
 )
