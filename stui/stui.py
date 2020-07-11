96644c11d9c6c2db07fff57ee6739c737b307ee2
Milad Alizadeh <milad2k+git@gmail.com>
2020-07-11 13:36:59 +0100

    Oops this should have been in previous commits

--- stui/stui.py
+++ stui/stui.py
@@ -427,30 +427,6 @@ class JobsTab(object):
         return self.view_placeholder
 
 
-class NodesTab(object):
-    def __init__(self, cluster):
-        super().__init__()
-
-        self.cluster = cluster
-
-        w = urwid.Text("Nodes: Under Construction ...")
-        w = urwid.Filler(w)
-
-        self.view = widgets.FancyLineBox(w, "Nodes")
-
-
-class AdminsTab(object):
-    def __init__(self, cluster):
-        super().__init__()
-
-        self.cluster = cluster
-
-        w = urwid.Text("Admin: Under Construction ...")
-        w = urwid.Filler(w)
-
-        self.view = widgets.FancyLineBox(w, "Admin")
-
-
 class StuiWidget(urwid.WidgetWrap):
     def __init__(self, cluster):
 
