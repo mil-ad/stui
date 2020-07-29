f394d8bbee43368e587499558600df5a7099c21a
Milad Alizadeh <milad2k+git@gmail.com>
2020-07-29 11:21:53 +0100

    Bumping version to 0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="A Slurm client for the terminal",
     long_description=README,
     long_description_content_type="text/markdown",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_packages(),
     author="Milad Alizadeh",
     url="https://github.com/mi-lad/stui",
