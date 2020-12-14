4d383c4d22ae982b18275c417164f720c878c7b7
Milad Alizadeh <hi@mil.ad>
2020-12-14 15:23:51 +0000

    ♻️ define version in the package

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import codecs
 from setuptools import setup, find_packages
+from stui import __version__
 
 with codecs.open("README.md", encoding="utf-8") as f:
     README = f.read()
@@ -9,7 +10,7 @@ setup(
     description="A Slurm client for the terminal",
     long_description=README,
     long_description_content_type="text/markdown",
-    version="0.3.2",
+    version=__version__,
     packages=find_packages(),
     author="Milad Alizadeh",
     url="https://github.com/mi-lad/stui",
