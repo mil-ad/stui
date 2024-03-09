3b93ab72ec4c5b302f22511748d53ead2ec79f01
Tianjian <57348932+EvoLandEco@users.noreply.github.com>
2024-03-09 18:04:51 +0100

    Remove debugging prints

--- stui/stui.py
+++ stui/stui.py
@@ -149,9 +149,6 @@ class StuiApp(object):
             pop_ups=False,
         )
 
-        print(f"MainLoop instance: {self.loop}")
-        print(f"watch_pipe available: {'watch_pipe' in dir(self.loop)}")
-
         self.fd = self.loop.watch_pipe(self.cluster_connect_callback)
         self.backend.connect(self.fd)
 
